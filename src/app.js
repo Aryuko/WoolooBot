edf12f2375952772795e33351a0b33d79ed7f2d7
Aryuko <peter.kjellen@gmail.com>
2019-09-09 20:21:11 +0200

    Add cooldown, improve command

--- src/app.js
+++ src/app.js
@@ -12,16 +12,24 @@ const config = require('../config.js')
 const client = new tmi.client(config)
 client.connect()
 
+var recentPokemonCommand = false
 client.on('chat', (channel, userstate, message, self) => {
+    let msg = message.toLowerCase()
     if(self) return;
 
-    if(message.toLowerCase().includes("!pokemon")){
-        // November 15, 2019
-        let str = timeToString(new Date('2019-11-15T03:00:00').getTime() - new Date().getTime())
-        client.say(channel, 'Pokémon Sword and Shield is released in ' + str)
+    if(msg.includes('when') && (msg.includes('pokemon') || msg.includes('pokémon'))){
+        let now = new Date().getTime()
+
+        if(!recentPokemonCommand || (now - recentPokemonCommand) > config.cooldown) {
+            // November 15, 2019
+            let str = timeToString(new Date('2019-11-15T03:00:00').getTime() - now)
+            client.say(channel, 'Pokémon Sword and Shield is released in ' + str)
+            recentPokemonCommand = now
+        }
     }
 })
 
+/* utils */
 function timeToString (millisec) {
 	var seconds = Math.floor(millisec / 1000);
 	var minutes = Math.floor(millisec / (1000 * 60));
