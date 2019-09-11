c7658610623ef2ece46535ea1e2a42b2eb1c05ad
Aryuko <pachimaryu@gmail.com>
2019-09-11 17:16:37 +0200

    Add base of summon command

--- src/app.js
+++ src/app.js
@@ -8,29 +8,64 @@
 
 const tmi = require('tmi.js')
 const config = require('../config.js')
+const fetch = require('node-fetch')
 
 const client = new tmi.client(config)
 client.connect()
 
 var recentPokemonCommand = false
+var recentSummon = false
+
 client.on('chat', (channel, userstate, message, self) => {
     if (self) { return }
     let msg = message.toLowerCase()
 
-    if(msg.includes('when') && (msg.includes('pokemon') || msg.includes('pokémon'))){
+    if (msg.includes('when') && (msg.includes('pokemon') || msg.includes('pokémon'))){
         let now = new Date().getTime()
 
-        if(!recentPokemonCommand || (now - recentPokemonCommand) > config.cooldown) {
+        if (!recentPokemonCommand || (now - recentPokemonCommand) > config.cooldown) {
             // November 15, 2019
             let releaseDate = new Date('2019-11-15T03:00:00').getTime()
             let str = timeToString(releaseDate - now)
             client.say(channel, 'Pokémon Sword and Shield is released in ' + str)
             recentPokemonCommand = now
         }
+    } else if (msg.includes('summon aryu') || msg.includes('aryu summon')) {
+        let now = new Date().getTime()
+        if(!recentSummon || (now - recentSummon) > config.cooldown) {
+            if (typeof aryuPresent == 'undefined') {
+                fetchChatters(channel).then((res) => {
+                    if(deepishContains(res.chatters, 'aryu')) {
+                        // Aryu is in chat
+                        client.say(channel, `She's already in chat!`)
+                    } else {
+                        // Aryu is not in chat
+                        client.say(channel, 'Attempting to summon Aryu 👀')
+                        
+                    }
+                })
+            }
+        }
     }
 })
 
+/* twitch api */
+function fetchChatters(channel) {
+    return fetch(`http://tmi.twitch.tv/group/user/${channel.replace(/#/, '')}/chatters`)
+    .then((response) => { 
+        return response.json()
+     })
+}
+
 /* utils */
+
+function deepishContains(object, key) {
+    for (k in object) {
+        if(object[k].includes(key)) { return true }
+    }
+    return false
+}
+
 function timeToString (millisec) {
 	var seconds = Math.floor(millisec / 1000)
 	var minutes = Math.floor(millisec / (1000 * 60))
