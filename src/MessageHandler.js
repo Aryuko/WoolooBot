09cf421fc3089e8d406dbcf111cae55d9775341f
Aryuko <pachimaryu@gmail.com>
2019-11-06 17:18:37 +0100

    Rework command handling

---
+++ src/MessageHandler.js
@@ -0,0 +1,163 @@
+const wh = require('webhook-discord')
+const fetch = require('node-fetch')
+
+module.exports = 
+class MessageHandler {
+    constructor (client, config) {
+        this.client = client
+        this.config = config
+
+        this.discordHook = new wh.Webhook(config.webhookUrl)
+
+        this.commandCooldowns = {}
+        this.commandState = {
+            summon: {
+                present: []
+            }
+        }
+    }
+
+    handleMessage (message, channel, userstate) {
+        let input = this.parseInput(message)
+        console.log(input)
+
+        if(input && input.command) {
+            switch (input.command) {
+                case 'releasedate':
+                case 'release':
+                case 'rd':
+                    this.releasedateCommand(input.args ? input.args[0] : '', channel)
+                    break;
+            
+                case 'summon':
+                    this.summonCommand(input.args ? input.args[0].toLowerCase() : '', channel, userstate.username)
+                    break;
+
+                default:
+                    break;
+            }
+        } else { /* Add alternative command invocations? like the msg.includes of old */ }
+    }
+
+    parseInput(message) {
+        let regex = new RegExp('\\' + this.config.commandPrefix + '(\\w+)(.*)', 'g')    // Capture one single word following the specified prefix
+        let result = regex.exec(message)
+        if (result) {
+            let args = result[2].trim().match(/(?:[^\s"']+|["'][^"']*["'])+/g);         // Split words by spaces, except when enclosed in quotes: https://stackoverflow.com/a/16261693/5621850
+            for (let index in args) { args[index].replace(/["']/g, '') }                // Remove quotes
+            return {
+                'command': result[1].toLowerCase(),
+                'args': args
+            }
+        } else { return false }
+    }
+
+    /* commands */
+
+    releasedateCommand(game, channel) {
+        let now = new Date().getTime()
+        if (!this.commandCooldowns.releasedate || (now - this.commandCooldowns.releasedate) > this.config.cooldown) {
+            let releaseDate = false
+            let gameString = false
+            switch (game) {
+                case 'pokemon':
+                case 'pokémon':
+                    // November 15, 2019
+                    releaseDate = new Date('2019-11-15T03:00:00').getTime()
+                    gameString = 'Pokémon Sword and Shield is released in '
+                    break;
+                    
+                    case 'ac':
+                    case 'animal crossing':
+                    // March 20, 2020
+                    releaseDate = new Date('2020-03-20T03:00:00').getTime()
+                    gameString = 'Animal Crossing: New Horizons is released in '
+                default:
+                    this.client.say(channel, `Invalid game D: List of valid games: 'ac', 'pokemon'`)
+                    return;
+            }
+            let timeString = this.timeToString(releaseDate - now)
+            this.client.say(channel, gameString + timeString)
+            this.commandCooldowns.releasedate = now
+        }
+    }
+
+    summonCommand(user, channel, invoker) {
+        console.log('present', this.commandState.summon.present)
+        let now = new Date().getTime()
+        if(!this.commandCooldowns.summon || (now - this.commandCooldowns.summon) > this.config.cooldown) {
+            switch (user) {
+                case 'aryu':
+                case '':
+                    if (!('aryu' in this.commandState.summon.present)) {
+                        this.fetchChatters(channel).then((res) => {
+                            if(this.deepishContains(res.chatters, 'aryu')) {
+                                // Aryu is in chat
+                                this.client.say(channel, `Aryu is already in chat! Try using a ping: @Aryu aryu5Aww`)
+                            } else {
+                                // Aryu is not in chat
+                                this.client.say(channel, 'Attempting to summon Aryu 👀')
+                                this.summonAryu(channel, invoker)
+                            }
+                        })
+                    }
+                    break;
+
+                default:
+                    this.client.say(channel, 'I only have permission to summon Aryu aryu5Cry')
+                    break;
+            }
+        }
+    }
+
+    /* utils */
+
+    deepishContains(object, key) {
+        for (let k in object) {
+            if(object[k].includes(key)) { return true }
+        }
+        return false
+    }
+
+    timeToString (millisec) {
+        var seconds = Math.floor(millisec / 1000)
+        var minutes = Math.floor(millisec / (1000 * 60))
+        var hours   = Math.floor(millisec / (1000 * 60 * 60))
+        var days    = Math.floor(millisec / (1000 * 60 * 60 * 24))
+        
+        let str
+
+        if      (seconds < 60)  { str = seconds + " seconds" }
+        else if (minutes < 60)  { str = minutes + " minutes" }
+        else if (hours < 24)    { str = hours + " hours" }
+        else                    { str = days + " days and " + hours % 24 + " hours" }
+        
+        if      (days < 1)      { str += ", THAT'S SOOOON!! FeelsAmazingMan aryu5Aww FeelsAmazingMan aryu5Aww" }
+        else if (days < 7)      { str += ", NOT LONG NOW! FeelsAmazingMan aryu5Aww" }
+        else if (days < 14)     { str += ", less than two weeks!! PogChamp" }
+        else if (days < 30)     { str += ", less than a month, not bad!" }
+        else if (days < 60)     { str += ", still quite a long time FeelsBadMan" }
+        else if (days > 60)     { str += ", that's over two months FeelsBadMan FeelsBadMan" }
+
+        return str
+    }
+
+    summonAryu(channel, username) {
+        let msg = new wh.MessageBuilder()
+            .setName("WoolooBot")
+            .setColor("#6441A4")
+            .setText("<@!106109569486315520>")
+            .addField("Aryu, you are being summoned!", `${username} wants you you to join https://www.twitch.tv/${channel.replace(/#/, '')}`)
+            .setTime()
+
+            this.discordHook.send(msg)
+    }
+
+    /* twitch api */
+    fetchChatters(channel) {
+        return fetch(`http://tmi.twitch.tv/group/user/${channel.replace(/#/, '')}/chatters`)
+        .then((response) => { 
+            return response.json()
+        })
+    }
+}
--- src/app.js
+++ src/app.js
@@ -8,109 +8,14 @@
 
 const tmi = require('tmi.js')
 const config = require('../config.js')
-const fetch = require('node-fetch')
-const wh = require('webhook-discord')
-
-const discordHook = new wh.Webhook(config.webhookUrl)
+const MessageHandler = require('./MessageHandler.js')
 
 const client = new tmi.client(config)
 client.connect()
-
-var recentPokemonCommand = false
-var recentACCommand = false
-var recentSummon = false
+let messageHandler = new MessageHandler(client, config)
 
 client.on('chat', (channel, userstate, message, self) => {
-    if (self) { return }
-    let msg = message.toLowerCase()
-
-    if (msg.includes('when') && (msg.includes('pokemon') || msg.includes('pokémon'))){
-        let now = new Date().getTime()
-
-        if (!recentPokemonCommand || (now - recentPokemonCommand) > config.cooldown) {
-            // November 15, 2019
-            let releaseDate = new Date('2019-11-15T03:00:00').getTime()
-            let str = timeToString(releaseDate - now)
-            client.say(channel, 'Pokémon Sword and Shield is released in ' + str)
-            recentPokemonCommand = now
-        }
-    } else if (msg.includes('when') && (msg.includes('animal crossing') || msg.includes('new horizons'))){
-        let now = new Date().getTime()
-
-        if (!recentACCommand || (now - recentACCommand) > config.cooldown) {
-            // March 20, 2020
-            let releaseDate = new Date('2020-03-20T03:00:00').getTime()
-            let str = timeToString(releaseDate - now)
-            client.say(channel, 'Animal Crossing: New Horizons is released in ' + str)
-            recentACCommand = now
-        }
-    } else if (msg.includes('summon aryu') || msg.includes('aryu summon')) {
-        let now = new Date().getTime()
-        if(!recentSummon || (now - recentSummon) > config.cooldown) {
-            if (typeof aryuPresent == 'undefined') {
-                fetchChatters(channel).then((res) => {
-                    if(deepishContains(res.chatters, 'aryu')) {
-                        // Aryu is in chat
-                        client.say(channel, `She's already in chat!`)
-                    } else {
-                        // Aryu is not in chat
-                        client.say(channel, 'Attempting to summon Aryu 👀')
-
-                        summonAryu(channel, userstate.username)
-                    }
-                })
-            }
-        }
+    if (!self && message.length > 1) {
+        messageHandler.handleMessage(message, channel, userstate)
     }
 })
-
-function summonAryu(channel, username) {
-    let msg = new wh.MessageBuilder()
-        .setName("WoolooBot")
-        .setColor("#6441A4")
-        .setText("<@!106109569486315520>")
-        .addField("Aryu, you are being summoned!", `${username} wants you you to join https://www.twitch.tv/${channel.replace(/#/, '')}`)
-        .setTime()
-
-        discordHook.send(msg)
-}
-
-/* twitch api */
-function fetchChatters(channel) {
-    return fetch(`http://tmi.twitch.tv/group/user/${channel.replace(/#/, '')}/chatters`)
-    .then((response) => { 
-        return response.json()
-     })
-}
-
-/* utils */
-
-function deepishContains(object, key) {
-    for (k in object) {
-        if(object[k].includes(key)) { return true }
-    }
-    return false
-}
-
-function timeToString (millisec) {
-	var seconds = Math.floor(millisec / 1000)
-	var minutes = Math.floor(millisec / (1000 * 60))
-	var hours   = Math.floor(millisec / (1000 * 60 * 60))
-    var days    = Math.floor(millisec / (1000 * 60 * 60 * 24))
-    
-    let str
-
-	if      (seconds < 60)  { str = seconds + " seconds" }
-	else if (minutes < 60)  { str = minutes + " minutes" }
-	else if (hours < 24)    { str = hours + " hours" }
-    else                    { str = days + " days and " + hours % 24 + " hours" }
-    
-    if      (days < 1)      { str += ", THAT'S SOOOON!! FeelsAmazingMan FeelsAmazingMan FeelsAmazingMan" }
-    else if (days < 7)      { str += ", NOT LONG NOW! FeelsAmazingMan" }
-    else if (days < 14)     { str += ", less than two weeks!! PogChamp" }
-    else if (days < 30)     { str += ", less than a month, not bad!" }
-    else if (days < 60)     { str += ", still quite a long time FeelsBadMan" }
-    else if (days > 60)     { str += ", that's over two months FeelsBadMan FeelsBadMan" }
-
-    return str
-}
